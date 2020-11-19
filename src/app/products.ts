60fd8b0724deb90e5de34c2a294ff51c9bf5b458
JosephEvans92 <dillon.evans90@yahoo.com>
2020-11-19 13:46:58 -0500

    1 file modified

--- src/app/products.ts
+++ src/app/products.ts
@@ -16,6 +16,74 @@ export const products = [
   }
 ];
 
+export const r1 = {
+  "profile":{
+  "firstName": "Joseph",
+  "lastName": "Evans",
+  "isAlive": true,
+  "age": 28,
+  "photo": "https://insuranceblog.accenture.com/wp-content/uploads/2019/06/Tech_Trends.jpg",
+  "profession": "IT technician",
+  "address": {
+    "streetAddress": "114-02 Francis Lewis Blvd",
+    "city": "New York ",
+    "state": "NY",
+    "postalCode": "11411",
+    "country": "USA",
+    "email": "jevans@qcc.cuny.edu"
+  },
+  "phoneNumbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+"languages":[
+	{
+		"language": "English",
+		"level": "100%"
+	},
+	
+		{
+		"language": "Spanish",
+		"level": "20%"
+	},
+			
+	
+	],
+
+"skills":[
+	{
+		"skill": "HTLM",
+		"level": "95%"
+	},
+	
+		{
+		"skill": "CSS",
+		"level": "80%"
+	},
+		{
+		"skill": "Linux",
+		"level": "85%"
+	},
+	
+		{
+		"skill": "Python",
+		"level": "50%"
+	}
+	],
+
+  },
+  
+  
+  "workExperience": [],
+  "education": []
+}
+
 
 /*
 Copyright Google LLC. All Rights Reserved.
