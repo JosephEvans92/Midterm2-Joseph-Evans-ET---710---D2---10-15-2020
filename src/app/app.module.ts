63fda76f12d5239046384832e1b1de4b6d1afe94
JosephEvans92 <dillon.evans90@yahoo.com>
2020-12-16 11:05:04 -0500

    7 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,8 @@
 import { HttpClientModule } from '@angular/common/http';
-
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { LightboxModule } from 'ngx-lightbox';
 
@@ -22,13 +25,6 @@ import {
   TooltipModule,
   TypeaheadModule,
 } from 'ngx-foundation';
-
-
-
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule } from '@angular/router';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -30,4 +30,5 @@ export class CartService {
   constructor(
     private http: HttpClient
   ) { }
+  
 }
